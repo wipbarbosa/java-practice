b45465ff3e7a0bed7111b2609de0a36fcb5f8327
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-17 16:20:18 -0300

    Atualização de portifólio

--- aula01/Main.java
+++ aula01/Main.java
@@ -1,7 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
-    }
 
+        String nome = "Guilherme";
+        int idade = 20;
+        double altura = 1.75;
+        boolean estudante = true;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(estudante);
+    }
 }
