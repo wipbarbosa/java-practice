2844f9778e9be806348edc9fc016794254343324
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-17 16:50:07 -0300

    Atualização de portifólio

--- aula01/Main.java
+++ aula01/Main.java
@@ -1,15 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
+        double numero1 = 10;
+        double numero2 = 20;
 
-        String nome = "Guilherme";
-        int idade = 20;
-        double altura = 1.75;
-        boolean estudante = true;
+        double soma = numero1 + numero2;
+        double subtracao = numero1 - numero2;
+        double multiplicacao = numero1 * numero2;
+        double divisao =  numero1 / numero2;
 
-        System.out.println(nome);
-        System.out.println(idade);
-        System.out.println(altura);
-        System.out.println(estudante);
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+
     }
 }
