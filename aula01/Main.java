72ddb958b7e776b9a65fa6fa095f2bdedf688cae
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-17 15:45:26 -0300

    cria primeiro programa Java

---
+++ aula01/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+        System.out.println("Meu nome é Guilherme.");
+        System.out.println("Estou aprendendo Java.");
+
+    }
+
+}
