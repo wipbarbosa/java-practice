ebff94cdf0a951018655b83ffa9df81437b147b7
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-26 16:01:08 -0300

    Atualização de portifólio

---
+++ aula01/EX03.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class EX03 {
+        public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o 1º número: ");
+        int numero1 = scanner.nextInt();
+
+        System.out.print("Digite o 2º número: ");
+        int numero2 = scanner.nextInt();
+
+        int adicao = numero1 + numero2;
+        int subtracao = numero1 - numero2;
+        double multiplicacao = numero1 * numero2;
+        double divisao = numero1 / numero2;
+
+        System.out.println("Adição: "+ adicao);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+    }
+}
