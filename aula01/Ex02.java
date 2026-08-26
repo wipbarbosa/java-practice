660077651047dc90622bf787a9961f51fc168df7
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-26 15:33:47 -0300

    Atualização de portifólio

---
+++ aula01/Ex02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex02 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.print("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Seu nome é: " + nome);
+        System.out.println("Sua idade é: " + idade);
+        System.out.println("Sua altura é: " + altura);
+    }
+}
