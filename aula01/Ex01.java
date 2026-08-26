0c36dfc7e0479067c232dcb708c57a0118c26593
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-26 14:54:45 -0300

    Atualização de portifólio

---
+++ aula01/Ex01.java
@@ -0,0 +1,11 @@
+public class Ex01 {
+
+    public static void main(String[] args) {
+
+        int idade = 21;
+        double altura = 1.80;
+
+        System.out.println("Minha idade é: " + idade);
+        System.out.println("Minha altura é: " + altura);
+    }
+}
