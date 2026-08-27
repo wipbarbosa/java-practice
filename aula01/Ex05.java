e35de8c2d1b8f9c08a3fccf0a18fb8ce1ee42ec6
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-27 17:57:33 -0300

    Atualização de portifólio

---
+++ aula01/Ex05.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex05 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+
+        if (idade <= 12) {
+            System.out.println("Criança");
+        } else if (idade <= 17){
+            System.out.println("Adolescente");
+        } else if (idade <= 59){
+            System.out.println("Adulto");
+        } else {
+            System.out.println("Idoso");
+        }
+    }
+}
