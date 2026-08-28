b0c334862f06f4e65898d4e734d21ec2a3b753b1
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-28 17:15:58 -0300

    Atualização de portifólio

--- aula02/Ex01.java
+++ aula02/Ex01.java
@@ -1,6 +1,23 @@
-package aula02;
 import java.util.Scanner;
 
 public class Ex01 {
 
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.print("Digite a sua renda: ");
+        double renda = scanner.nextDouble();
+
+        if (idade < 0 || renda < 0) {
+            System.out.println("Dados invalidos");
+        } else if (idade >= 18 && renda <= 2000) {
+            System.out.println("Voce pode receber o beneficio");
+        } else {
+            System.out.println("Voce nao pode receber o beneficio");
+        }
+    }
 }
