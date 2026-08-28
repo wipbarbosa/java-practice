c8075860bad54ad53ae8c68b766ec42be3aa6716
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-28 17:51:45 -0300

    Atualização de portifólio

---
+++ aula02/Ex03.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ex03 {
+
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero inteiro");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0){
+            System.out.println("O número é par");
+        } else {
+            System.err.println("O numerop é impar");
+        }
+    }
+
+    
+    
+}
