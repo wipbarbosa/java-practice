6dea0a9fb1e05638bbd192768dcacb481d27fb54
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-09-01 17:55:15 -0300

    Atualização de portifólio

---
+++ aula02/Ex04.java
@@ -0,0 +1,13 @@
+public class Ex04 {
+
+    public static void main(String[] args) {
+
+        int numero = 0;
+        
+        while (numero < 10){
+            System.out.println(numero);
+            numero++;
+        }
+            
+    }
+}
